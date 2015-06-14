1a8d5bcd5d31978777e1901e5deb07a00f430c37
Danny McClanahan <danieldmcclanahan@gmail.com>
2015-06-13 21:19:44 -0700

    fixed switch-grep

--- switch-grep.sh
+++ switch-grep.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
 
-# switches the locations of the last two grep arguments, and adds a . if wanted
-
-
-
-numNonOption=0
-for arg in $@; do
-  if [ "$(echo $arg | cut -b1)" != "-" ]; then
-    ((++numNonOption))
-  fi
-done
-
+# switches the locations of the last two grep arguments, and uses colorized
+# output if allowed
 useColorArg=$(grep --color 2>&1 | grep -- --color >/dev/null && \
                 echo "" || echo "--color")
-
-cmd="grep -rnH --binary-files=without-match $useColorArg"
-if [ $numNonOption -gt 1 ]; then
-  $cmd $@
-else
-  $cmd $@ .
-fi
+grep -rnH --binary-files=without-match $useColorArg $@ .
