1c7459121f685e362fed658228aef4f6dbfdaf90
Danny McClanahan <danieldmcclanahan@gmail.com>
2016-04-04 06:06:53 -0500

    finish jison-flex

--- integrations/jison-mode.el
+++ integrations/jison-mode.el
@@ -1,5 +1,9 @@
 (defvar jison-flex-keywords
   '(("/\\*.*?\\*/" . font-lock-comment-face)
+    ("\\({\\)\\([a-zA-Z][a-zA-Z0-9]*\\)\\(}\\)" .
+     ((1 font-lock-preprocessor-face)
+      (2 font-lock-variable-name-face)
+      (3 font-lock-preprocessor-face)))
     ("^\\(%\\)\\([a-zA-Z][_a-zA-Z0-9]*\\)\\s-+?\\(.*\\)$" .
      ((1 font-lock-preprocessor-face)
       (2 font-lock-keyword-face)
@@ -7,12 +11,18 @@
     ("^\\(%%\\)\\(.*\\)$" .
      ((1 font-lock-preprocessor-face)
       (2 font-lock-warning-face)))
-    ("\\(?:\\`\\|[^\\]\\)\\(\\(?:\\\\\\\\\\)*\\)\\({\\|}\\|\\[\\|\\]\\|(\\|)\\|\"\\|'\\)"
+    ("^[a-zA-Z][a-zA-Z0-9]*" . ((0 font-lock-function-name-face)))
+    ("\\(?:\\`\\|[^\\]\\)\\(\\(?:\\\\\\\\\\)*\\)\\(\\(?:{\\|}\\|\\[\\|\\]\\|(\\|)\\|\"\\|'\\|\\*\\|\\+\\|\\^\\|,\\|\\-\\||\\|\\.\\|\\?\\)+\\)"
      . ((1 font-lock-preprocessor-face)
         (2 font-lock-keyword-face)))
-    ("\\(?:{\\|}\\|\\[\\|\\]\\|(\\|)\\|\"\\|'\\)\\({\\|}\\|\\[\\|\\]\\|(\\|)\\|\"\\|'\\)" .
-     ((1 font-lock-keyword-face)))
-    ("\\\\+." . ((0 font-lock-variable-name-face)))))
+    ("\\(\\(?:\\\\\\\\\\)+\\)\\(\\(?:{\\|}\\|\\[\\|\\]\\|(\\|)\\|\"\\|'\\|\\*\\|\\+\\|\\^\\|,\\|\\-\\||\\|\\.\\|\\?\\)+\\)"
+     . ((1 font-lock-preprocessor-face)
+        (2 font-lock-keyword-face)))
+    ("\\\\+." . ((0 font-lock-type-face)))
+    ("\\(?:{\\|}\\|\\[\\|\\]\\|(\\|)\\|\"\\|'\\|\\*\\|\\+\\|\\^\\|,\\|\\-\\||\\|\\.\\|\\?\\)"
+     . ((0 font-lock-keyword-face)))
+    ("\\<return\\>" . ((0 font-lock-type-face)))
+    ("\\<yytext\\>" . ((0 font-lock-variable-name-face)))))
 
 (define-derived-mode jison-flex-mode fundamental-mode "JFLex"
   "A mode to highlight flex keywords for use in Jison."
