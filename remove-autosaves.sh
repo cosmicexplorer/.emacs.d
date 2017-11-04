9306a0d2c4be4c9bedc88b8ccf3fa1b27be1c613
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2017-11-03 19:40:46 -0700

    fix remove-autosaves.sh

--- remove-autosaves.sh
+++ remove-autosaves.sh
@@ -5,7 +5,7 @@ set -e
 for dir in auto-save-files auto-save-list undo-tree-history backup-files; do
   rm -rf "$dir"
   mkdir "$dir"
-  echo "*\n!.gitignore" > "$dir/.gitignore"
+  echo -e "*\n!.gitignore" > "$dir/.gitignore"
 done
 
 rm -f .emacs.desktop
