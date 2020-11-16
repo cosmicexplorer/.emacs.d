2d07171ba8293555d66ba828ca6f668f2499b958
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2020-11-15 16:20:21 -0800

    fix pre-commit script and update themes

--- disallow-adding-faces.zsh
+++ disallow-adding-faces.zsh
@@ -2,14 +2,45 @@
 
 set -euxo pipefail
 
-function get-added-faces-heuristic {
-  git diff HEAD \
-    | sed -Ene "s#^\\+ '\\(([^s]+) \\(\\(t \\(.*\$#\\0#gp"
+function die {
+  local -r exit_code="$1"
+  printf '%s\n' >&2 "Failed with code $exit_code:" "${@:2}"
+  exit "$exit_code"
 }
 
-get-added-faces-heuristic
-get-added-faces-heuristic \
-  | wc -l \
-  | if [[ "$(cat)" -ne 0 ]]; then
-  exit 1
+function diff-staged-files {
+  git diff HEAD
+}
+
+# NB: dead code
+function get-added-faces-heuristic {
+  diff-staged-files \
+    | grep -E "^\+ '\\(([^s]+) \\(\\(t \\(.*\$"
+}
+
+function filter-out-selected-packages {
+  grep -vF 'package-selected-packages'
+}
+
+function any-added-content-heuristic {
+  diff-staged-files \
+    | grep -E "^\+ '\\(" \
+    | filter-out-selected-packages
+}
+
+function is-empty-array {
+  [[ "$#" -eq 1 && "$1" == '' ]]
+}
+
+local -a all_results=( "$(any-added-content-heuristic)" )
+if is-empty-array "${all_results[@]}"; then
+  all_results=( )
+fi
+
+if [[ "${#all_results[@]}" -ne 0 ]] ; then
+  die "${#all_results[@]}" \
+      "${#all_results[@]} faces or variables were added:" \
+      "${all_results[@]}"
+else
+  exit 0
 fi
