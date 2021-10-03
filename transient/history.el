35ff94ed33c36af0a4337748cef19745f98bfe16
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2021-10-03 13:44:18 -0400

    transient history (?)

--- transient/history.el
+++ transient/history.el
@@ -6,14 +6,20 @@
   ("--ff")
   nil)
  (magit-commit nil
+               ("--reset-author")
                ("--no-verify" "--reuse-message=ORIG_HEAD"))
  (magit-diff
-  ("--no-ext-diff" "--stat"))
+  ("--no-ext-diff" "--stat")
+  (("--" "lib/spack/env/cc")
+   "--no-ext-diff" "--stat"))
  (magit-dispatch nil)
  (magit-fetch nil)
  (magit-gitignore nil)
  (magit-log
   ("-n256" "--graph" "--color" "--decorate" "--stat")
+  ("-n256"
+   ("--" "lib/spack/env/cc")
+   "--graph" "--color" "--decorate" "--stat")
   ("-n256" "--graph" "--color" "--decorate")
   ("-n256" "--graph" "--decorate")
   ("-n256" "-Sgpg" "--graph" "--decorate")
@@ -21,8 +27,7 @@
   ("-n256" "-Spinentry" "--graph" "--decorate")
   ("-n256" "--graph" "--color" "--decorate" "--show-signature" "--stat")
   ("-n256" "--simplify-by-decoration" "--graph" "--color" "--decorate" "--show-signature" "--stat")
-  ("-n256" "--color" "--decorate" "--show-signature" "--stat")
-  ("-n256" "--color" "--decorate" "--show-signature" "--patch"))
+  ("-n256" "--color" "--decorate" "--show-signature" "--stat"))
  (magit-log:-S "gpg" "pin" "pinentry")
  (magit-margin-settings nil)
  (magit-merge nil)
@@ -30,10 +35,15 @@
              ("--rebase"))
  (magit-push nil
              ("--force-with-lease"))
- (magit-rebase nil)
+ (magit-rebase
+  ("--autostash")
+  nil
+  ("--autostash" "--interactive"))
  (magit-remote
   ("-f"))
  (magit-revert
-  ("--edit"))
+  ("--edit")
+  nil)
  (magit-revision-history "ORIG_HEAD")
- (magit-stash nil))
+ (magit-stash nil)
+ (magit:-- "lib/spack/env/cc"))
