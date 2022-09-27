960d0fe1d779fa7e109f91f3a44f12e78b641c19
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2022-09-26 23:10:40 -0400

    kill elpa elc in byte compile cleanup script too

--- remove-byte-compiled.sh
+++ remove-byte-compiled.sh
@@ -3,8 +3,7 @@
 set -euxo pipefail
 
 find . \
-  -not -path '*elpa*' \
-  -and \( -name '*.elc' -or -name 'gmon.out' \) \
+  \( -name '*.elc' -or -name 'gmon.out' \) \
   -exec rm -v {} '+'
 
 rm -rfv ./eln-cache
