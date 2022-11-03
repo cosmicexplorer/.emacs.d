745cb29f85b5267ae703c1f5fafd55978a32338b
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2022-11-03 08:49:32 -0400

    add notes to byte compile removal script

--- remove-byte-compiled.sh
+++ remove-byte-compiled.sh
@@ -2,9 +2,19 @@
 
 set -euxo pipefail
 
+# This script removes compiled output *only for the scripts loaded in
+# .emacs.d!* The emacs build itself will byte/native-compile the lisp
+# code in emacs itself.
+
+# Remove byte-compiled output.
 find . \
   \( -name '*.elc' -or -name 'gmon.out' \) \
   -exec rm -v {} '+'
 
+# Remove native-compiled output.
+# NB: this shouldn't be necessary, as the eln cache is already segregated by
+#     version, but currently when different emacs versions are used, they will
+#     attempt to use native code from prior versions, which causes init to fail
+#     with unrecognized function names e.g. `defvar-1'. Very strange.
 rm -rfv ./eln-cache
 
